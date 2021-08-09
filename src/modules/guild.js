2bd22d09d3bbf8c9295c4e2b1765471206a8cafa
JacobLinCool <jacoblincool@gmail.com>
2021-08-09 17:28:25 +0800

    v0.6.7b

--- src/modules/guild.js
+++ src/modules/guild.js
@@ -3,25 +3,34 @@ exports.parameters = [];
 exports.run = async ({ page, outputs, catchError, log }) => {
     if (!outputs.login || !outputs.login.success) throw new Error("使用者未登入，無法進行公會簽到");
 
-    await page.goto("https://home.gamer.com.tw/joinGuild.php");
-    await page.waitForTimeout(2000);
-    const guilds = await page.evaluate(() => {
-        return [...document.querySelectorAll(".acgbox .acgboximg a")].map((a) => a.href);
-    });
-    log(`已加入 ${guilds.length} 個公會`);
-
-    for (let guild of guilds) {
+    let retry = 3;
+    while (retry--) {
         try {
-            await page.goto(guild);
-            await page.waitForTimeout(1000);
-            const name = await page.evaluate(() => {
-                guild.sign();
-                return document.querySelector(".main-container_header_info h1").innerText;
-            });
+            await page.goto("https://home.gamer.com.tw/joinGuild.php");
             await page.waitForTimeout(2000);
-            log(`已簽到 ${name}`);
+            const guilds = await page.evaluate(() => {
+                return [...document.querySelectorAll(".acgbox .acgboximg a")].map((a) => a.href);
+            });
+            log(`已加入 ${guilds.length} 個公會`);
+
+            for (let guild of guilds) {
+                try {
+                    await page.goto(guild);
+                    await page.waitForTimeout(1000);
+                    const name = await page.evaluate(() => {
+                        guild.sign();
+                        return document.querySelector(".main-container_header_info h1").innerText;
+                    });
+                    await page.waitForTimeout(2000);
+                    log(`已簽到 ${name}`);
+                } catch (err) {
+                    catchError(err);
+                }
+            }
+            break;
         } catch (err) {
             catchError(err);
+            await page.waitFor(500);
         }
     }
 
@@ -30,6 +39,6 @@ exports.run = async ({ page, outputs, catchError, log }) => {
 
 function report({}) {
     let body = `# 公會簽到\n\n`;
-    body += `已執行\n\n`;
+    body += `🟢 已執行\n\n`;
     return body;
 }
