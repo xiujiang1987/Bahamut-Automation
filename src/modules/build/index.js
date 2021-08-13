0f452a506597b43ee801cef90355e6339a94f72e
JacobLinCool <jacoblincool@gmail.com>
2021-08-13 18:23:07 +0800

    測試 build 模組

---
+++ src/modules/build/index.js
@@ -0,0 +1,72 @@
+exports.parameters = [
+    {
+        name: "build",
+        required: true,
+        example: [{ bsn: "", snA: "", content: "" }],
+    },
+];
+
+exports.run = async ({ page, outputs, params, catchError, log }) => {
+    if (!outputs.login || !outputs.login.success) throw new Error("使用者未登入，無法發佈勇者大聲說");
+
+    const { build } = params;
+    if (build.length < 1) return { success: false };
+
+    for (let i = 0; i < build.length; i++) {
+        try {
+            const { bsn, snA, content } = build[i];
+            await page.goto(`https://forum.gamer.com.tw/post1.php?bsn=${bsn}&snA=${snA}&type=2`);
+            await page.waitForTimeout(2000);
+            await page.evaluate(() => {
+                onTipsClick();
+            });
+            await page.waitForTimeout(300);
+            await page.evaluate(() => {
+                bahaRte.toolbar.alternateView(true);
+            });
+            await page.waitForTimeout(300);
+            await page.type("#source", replace(content), { delay: 11 });
+            await page.waitForTimeout(300);
+            await page.evaluate(() => {
+                Forum.Post.post();
+            });
+            await page.waitForTimeout(300);
+            await page.click("form[method=dialog] button[type=submit]");
+            await page.waitForTimeout(2000);
+            if (!page.url().includes("last=1")) {
+                throw new Error("發生未知錯誤");
+            }
+        } catch (err) {
+            catchError(err);
+        }
+    }
+
+    return { success: true };
+};
+
+function replace(str) {
+    const t = time();
+    const rules = [
+        [/\$time\$/g, `$year$/$month$/$day$ $hour$:$minute$:$second$`],
+        [/\$year\$/g, t[0]],
+        [/\$month\$/g, t[1]],
+        [/\$day\$/g, t[2]],
+        [/\$hour\$/g, t[3]],
+        [/\$minute\$/g, t[4]],
+        [/\$second\$/g, t[5]],
+    ];
+
+    for (let i = 0; i < rules.length; i++) str = str.replace(rules[i][0], rules[i][1]);
+
+    return str;
+}
+
+function time() {
+    const date = new Date().toLocaleString("en", { timeZone: "Asia/Taipei" }).split(", ");
+    let [month, day, year] = date[0].split("/");
+    let [hour, minute, second] = date[1].match(/\d{1,2}/g);
+
+    if (+hour === 12 && date[1].toLowerCase().includes("am")) hour = String(+hour - 12);
+    if (+hour < 12 && date[1].toLowerCase().includes("pm")) hour = String(+hour + 12);
+    return [year, month, day, hour, minute, second];
+}
