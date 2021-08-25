135a614bff3367ef15ab75ab1049e62da2f8d5bb
JacobLinCool <jacoblincool@users.noreply.github.com>
2021-08-25 04:45:10 +0000

    CircleCI Ignore: [ci skip] 9336f440c630faa046069b8d9554fddf792d48ca

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const { catchFatal, catchError, indentedCatchError } = require("./error");
 const { log, indentedLog } = require("./log");
 const { Browser, Page } = require("./browser");
 
-const VERSION = "v0.6.8";
+const VERSION = "v0.6.9";
 
 async function main({ config = {}, modules = [], ...params }) {
     try {
