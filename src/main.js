e39aeef1e2ba37d8d2fbb5098a7cdb62ace034e8
JacobLinCool <jacoblincool@gmail.com>
2021-09-08 00:14:04 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const { catchFatal, catchError, indentedCatchError } = require("./error");
 const { log, indentedLog } = require("./log");
 const { Browser, Page } = require("./browser");
 
-const VERSION = "v0.6.9";
+const VERSION = "v0.6.10";
 
 async function main({ config = {}, modules = [], ...params }) {
     try {
