c5a7934a7abd4d4a3534c544c68acf083757e0a2
JacobLinCool <jacoblincool@gmail.com>
2021-12-01 01:26:36 +0800

    refactor(module): report module ts rewrite

--- src/modules/report/index.ts
+++ src/modules/report/index.ts
@@ -1,8 +1,22 @@
+import { convert as html_to_text } from "html-to-text";
+import Module from "../_module";
+
+type Config = {
+    title: string;
+    ignore: string | string[];
+};
+
 const md = require("markdown-it")();
 const td = new (require("turndown"))({ headingStyle: "atx" });
-const { convert: html_to_text } = require("html-to-text");
 
-exports.parameters = [
+const DEFAULT_CONFIG: Config = {
+    title: "巴哈自動化！ 報告 $time$",
+    ignore: ["login", "logout", "report"],
+};
+
+const report = new Module();
+
+report.parameters = [
     {
         name: "report_title",
         required: false,
@@ -13,17 +27,11 @@ exports.parameters = [
     },
 ];
 
-const DEFAULT_CONFIG = {
-    title: "巴哈自動化！ 報告 $time$",
-    ignore: ["login", "logout", "report"],
-};
+report.run = async ({ params, outputs, logger }) => {
+    const log = (...args: any[]) => logger.log("\u001b[95m[報告]\u001b[m", ...args);
 
-exports.run = async ({ params, outputs, logger }) => {
-    const log = (...args) => logger.log("\u001b[95m[報告]\u001b[m", ...args);
+    const config: Config = Object.assign({}, DEFAULT_CONFIG, JSON.parse(JSON.stringify(params)));
 
-    const config = Object.assign({}, DEFAULT_CONFIG, JSON.parse(JSON.stringify(params)));
-
-    if (typeof config.labels === "string") config.labels = config.labels.split(",");
     if (typeof config.ignore === "string") config.ignore = config.ignore.split(",");
 
     log("DONE");
@@ -36,23 +44,23 @@ exports.run = async ({ params, outputs, logger }) => {
     };
 };
 
-async function text(outputs, config) {
+async function text(outputs: any, config: Config) {
     const { html } = await normalize(outputs, config);
     const text = html_to_text(html).replace(/\n\n\n+/g, "\n\n");
     return text;
 }
 
-async function markdown(outputs, config) {
+async function markdown(outputs: any, config: Config) {
     const { markdown } = await normalize(outputs, config);
     return markdown;
 }
 
-async function html(outputs, config) {
+async function html(outputs: any, config: Config) {
     const { html } = await normalize(outputs, config);
     return html;
 }
 
-async function normalize(outputs, config) {
+async function normalize(outputs: any, config: Config) {
     let report = "";
 
     report += `# ${config.title}\n\n`;
@@ -89,9 +97,9 @@ async function normalize(outputs, config) {
     return { html, markdown };
 }
 
-function replace(str) {
+function replace(str: string) {
     const t = time();
-    const rules = [
+    const rules: [RegExp, string][] = [
         [/\$time\$/g, `$year$/$month$/$day$ $hour$:$minute$:$second$`],
         [/\$year\$/g, t[0]],
         [/\$month\$/g, t[1]],
@@ -101,12 +109,14 @@ function replace(str) {
         [/\$second\$/g, t[5]],
     ];
 
-    for (let i = 0; i < rules.length; i++) str = str.replace(rules[i][0], rules[i][1]);
+    for (let i = 0; i < rules.length; i++) {
+        str = str.replace(rules[i][0], rules[i][1]);
+    }
 
     return str;
 }
 
-function time() {
+function time(): string[] {
     const date = new Date().toLocaleString("en", { timeZone: "Asia/Taipei" }).split(", ");
     let [month, day, year] = date[0].split("/");
     let [hour, minute, second] = date[1].match(/\d{1,2}/g);
@@ -115,3 +125,5 @@ function time() {
     if (+hour < 12 && date[1].toLowerCase().includes("pm")) hour = String(+hour + 12);
     return [year, month, day, hour, minute, second];
 }
+
+export default report;
