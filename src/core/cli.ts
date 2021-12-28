b83b62d17fcb7d772ae28b55c0715436250bf26c
JacobLinCool <jacoblincool@gmail.com>
2021-12-29 02:21:53 +0800

    fix: cli trim inputs

--- src/core/cli.ts
+++ src/core/cli.ts
@@ -17,17 +17,18 @@ async function main() {
     let mode = (args["mode"] ? +args["mode"][0] : null) || (args["m"] ? +args["m"][0] : null);
     if (mode !== 1 && mode !== 2) {
         while (true) {
-            mode = +(await ask(["選擇模式: ", "1. 設定檔執行", "2. 直接執行", ">> "].join("\n")));
+            mode = +(await ask(["選擇模式: ", "1. 設定檔執行", "2. 直接執行", ">> "].join("\n"))).trim();
             if (mode === 1 || mode === 2) break;
         }
     }
 
     if (mode === 1) {
         let config_path = (args["config"] ? args["config"][0] : null) || (args["c"] ? args["c"][0] : null);
+        config_path = remove_quotes(config_path);
         if (config_path) config_path = path.resolve(process.cwd(), config_path);
         if (!fs.existsSync(config_path)) {
             while (true) {
-                config_path = path.resolve(process.cwd(), await ask("請輸入設定檔位置: "));
+                config_path = path.resolve(process.cwd(), remove_quotes((await ask("請輸入設定檔位置: ")).trim()));
                 if (fs.existsSync(config_path)) break;
                 console.log("設定檔不存在:", config_path);
             }
@@ -64,11 +65,18 @@ function parsed_args(): { [key: string]: any } {
             now = arg.slice(1);
             if (!parsed[now]) parsed[now] = [];
         } else if (now) {
-            parsed[now].push(arg);
+            parsed[now].push(arg.trim());
         }
     }
 
     return parsed;
+}
+
+function remove_quotes(str: string): string {
+    if (str && ((str.startsWith('"') && str.endsWith('"')) || (str.startsWith("'") && str.endsWith("'")))) {
+        return str.substring(1, str.length - 1);
+    }
+    return str;
 }
 
 function help() {
