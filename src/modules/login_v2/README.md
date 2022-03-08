20b0670f28b6bb4a8a9890cf08401740f128d086
Dxball <dxball@users.noreply.github.com>
2022-03-09 01:28:23 +0800

    add login_v2 module
    login_v2 will get login cookies by mobile app api without recaptcha

---
+++ src/modules/login_v2/README.md
@@ -0,0 +1,17 @@
+# Login V2
+
+- Module Name: `login_v2`
+
+## 參數
+
+必要參數
+- `username` - 帳號
+- `password` - 密碼
+
+可選參數
+- `twofa` - 兩步驗驗證碼
+- `login_max_attempts` - 最多登入失敗次數
+
+### 使用方法
+
+請將 `login` 換成 `login_v2` 即可
--- src/modules/utils/index.ts
+++ src/modules/utils/index.ts
@@ -2,7 +2,10 @@ export default {
     parameters: [],
     run: async ({ outputs }) => {
         return {
-            logged_in: () => (outputs.login && outputs.login.success) || outputs.cookie_login,
+            logged_in: () =>
+                (outputs.login && outputs.login.success) ||
+                outputs.cookie_login ||
+                (outputs.login_v2 && outputs.login_v2.success),
         };
     },
 };
