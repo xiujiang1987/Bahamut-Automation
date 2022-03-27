9399111b3f245d4f1edc2ba50a2c86073d03eb01
JacobLinCool <jacoblincool@users.noreply.github.com>
2022-03-27 16:48:40 +0000

    deploy: d384ed25f5599e4e692e127c450320362af228ff

--- action.js
+++ action.js
@@ -1,2 +1,65 @@
-var t=require("child_process");p();l();function p(){try{process.stdout.write("Installing PNPM... "),(0,t.execSync)("npm i -g pnpm",{cwd:__dirname}),console.log("Done")}catch{}try{process.stdout.write("Installing Packages... "),(0,t.execSync)("pnpm i",{cwd:__dirname}),console.log("Done")}catch{}try{process.stdout.write("Installing Playwright Dependencies... "),(0,t.execSync)("pnpx -y playwright install",{cwd:__dirname}),console.log("Done")}catch{}try{process.stdout.write("Installing Browser Dependencies... "),(0,t.execSync)("pnpx -y playwright install-deps",{cwd:__dirname}),console.log("Done")}catch{}console.log(`
-`)}async function l(){let e=require("@actions/core");try{let{BahamutAutomation:s}=require("./lib/core"),o=e.getInput("modules"),n={...JSON.parse(e.getInput("parameters")||"{}")},c={...JSON.parse(e.getInput("secrets")||"{}")},a={type:"webkit",...JSON.parse(e.getInput("browser")||"{}")},r=await new s({modules:o.split(",").map(i=>i.trim()),params:{...n,...c},browser:a}).run();r&&(console.log(r),process.exit(0))}catch(s){e.setFailed(s.message),process.exit(1)}}
+// node_modules/.pnpm/tsup@5.11.13_typescript@4.5.2/node_modules/tsup/assets/esm_shims.js
+import { fileURLToPath } from "url";
+import path from "path";
+var getFilename = () => fileURLToPath(import.meta.url);
+var getDirname = () => path.dirname(getFilename());
+var __dirname = /* @__PURE__ */ getDirname();
+
+// src/action/action.ts
+import { execSync } from "node:child_process";
+import fs from "node:fs";
+import path2 from "node:path";
+prepare();
+main();
+function prepare() {
+  try {
+    process.stdout.write("Installing PNPM... ");
+    execSync("npm i -g pnpm", { cwd: __dirname });
+    console.log("Done");
+  } catch (err) {
+  }
+  try {
+    process.stdout.write("Installing Packages... ");
+    execSync("pnpm i", { cwd: __dirname });
+    console.log("Done");
+  } catch (err) {
+  }
+  try {
+    process.stdout.write("Installing Playwright Dependencies... ");
+    execSync("pnpx -y playwright install", { cwd: __dirname });
+    console.log("Done");
+  } catch (err) {
+  }
+  try {
+    process.stdout.write("Installing Browser Dependencies... ");
+    execSync("pnpx -y playwright install-deps", { cwd: __dirname });
+    console.log("Done");
+  } catch (err) {
+  }
+  console.log("\n");
+}
+async function main() {
+  const core = (await import("@actions/core")).default;
+  try {
+    const config_path = path2.resolve(core.getInput("config"));
+    const secrets = { ...JSON.parse(core.getInput("secrets") || "{}") };
+    let raw = fs.readFileSync(config_path, "utf8");
+    for (const key in secrets) {
+      const regex = new RegExp("\\$" + key, "g");
+      console.log(`Replacing ${raw.match(regex)?.length ?? 0} ${regex} in ${config_path}`);
+      raw = raw.replace(regex, secrets[key]);
+    }
+    fs.writeFileSync(config_path, raw);
+    const { BahamutAutomation } = await import("./lib/core/index.js");
+    const automation = BahamutAutomation.from(config_path);
+    automation.setup_listeners();
+    const result = await automation.run();
+    if (result) {
+      console.log(result);
+      process.exit(0);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+    process.exit(1);
+  }
+}
