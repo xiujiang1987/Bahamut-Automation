a77a5a6d2aedd933821872b9c13b3e26c3d67890
JacobLinCool <jacoblincool@gmail.com>
2022-05-24 03:07:44 +0800

    build: Docker image for both `arm64` and `amd64`

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,1 +1,2 @@
-docker build . -t jacoblincool/bahamut-automation
+docker buildx build --push --platform linux/arm64/v8,linux/amd64 --tag jacoblincool/bahamut-automation .
+# docker build -t jacoblincool/bahamut-automation .
