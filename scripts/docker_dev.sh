a1375a67bb9aa90abac11bd38d00491830aa6a2b
JacobLinCool <jacoblincool@gmail.com>
2022-07-10 21:50:24 +0800

    build: add sha to docker dev build

--- scripts/docker_dev.sh
+++ scripts/docker_dev.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker buildx build --push --platform linux/arm64/v8,linux/amd64 -t jacoblincool/bahamut-automation:dev .
+commit=$(git rev-parse --short head)
+docker buildx build --push --platform linux/arm64/v8,linux/amd64 -t "jacoblincool/bahamut-automation:$commit" -t jacoblincool/bahamut-automation:dev .
 
 # docker build -t jacoblincool/bahamut-automation .
