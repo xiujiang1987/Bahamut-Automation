4bac6b54cd2fdef719dddbed64caa0233e4716ec
JacobLinCool <jacoblincool@gmail.com>
2022-07-11 15:45:03 +0800

    build: checkout dev head commit in main

--- scripts/docker_dev.sh
+++ scripts/docker_dev.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-commit=$(git rev-parse --short head)
+commit=$(git rev-parse --short main)
 docker buildx build --push --platform linux/arm64/v8,linux/amd64 -t "jacoblincool/bahamut-automation:$commit" -t jacoblincool/bahamut-automation:dev .
 
 # docker build -t jacoblincool/bahamut-automation .
