9ea24ff605743249df8512a2ef3bc0fb5ebcc929
JacobLinCool <jacoblincool@gmail.com>
2022-07-28 06:12:24 +0800

    build: push then pull image tags

--- scripts/docker_dev.sh
+++ scripts/docker_dev.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-docker buildx bake --load --set "*.platform=linux/arm/v7,linux/arm64/v8,linux/amd64"
+docker buildx bake --push --set "*.platform=linux/arm/v7,linux/arm64/v8,linux/amd64" --file docker-compose.dev.yml
 commit=$(git rev-parse --short main)
-docker tag "jacoblincool/bahamut-automation:pure" "jacoblincool/bahamut-automation:pure-$commit"
-docker tag "jacoblincool/bahamut-automation:firefox" "jacoblincool/bahamut-automation:firefox-$commit"
-docker tag "jacoblincool/bahamut-automation:chromium" "jacoblincool/bahamut-automation:chromium-$commit"
-docker tag "jacoblincool/bahamut-automation:webkit" "jacoblincool/bahamut-automation:webkit-$commit"
-docker tag "jacoblincool/bahamut-automation:all" "jacoblincool/bahamut-automation:all-$commit"
+docker pull jacoblincool/bahamut-automation:dev-pure
+docker pull jacoblincool/bahamut-automation:dev-firefox
+docker pull jacoblincool/bahamut-automation:dev-chromium
+docker pull jacoblincool/bahamut-automation:dev-webkit
+docker pull jacoblincool/bahamut-automation:dev-all
+docker tag "jacoblincool/bahamut-automation:dev-pure" "jacoblincool/bahamut-automation:pure-$commit"
+docker tag "jacoblincool/bahamut-automation:dev-firefox" "jacoblincool/bahamut-automation:firefox-$commit"
+docker tag "jacoblincool/bahamut-automation:dev-chromium" "jacoblincool/bahamut-automation:chromium-$commit"
+docker tag "jacoblincool/bahamut-automation:dev-webkit" "jacoblincool/bahamut-automation:webkit-$commit"
+docker tag "jacoblincool/bahamut-automation:dev-all" "jacoblincool/bahamut-automation:all-$commit"
 docker push --all-tags jacoblincool/bahamut-automation
--- scripts/docker_release.sh
+++ scripts/docker_release.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-docker buildx bake --load --set "*.platform=linux/arm/v7,linux/arm64/v8,linux/amd64"
+docker buildx bake --push --set "*.platform=linux/arm/v7,linux/arm64/v8,linux/amd64"
 version=$(git describe --tags --abbrev=0)
+docker pull jacoblincool/bahamut-automation:pure
+docker pull jacoblincool/bahamut-automation:firefox
+docker pull jacoblincool/bahamut-automation:chromium
+docker pull jacoblincool/bahamut-automation:webkit
+docker pull jacoblincool/bahamut-automation:all
 docker tag "jacoblincool/bahamut-automation:pure" "jacoblincool/bahamut-automation:pure-$version"
 docker tag "jacoblincool/bahamut-automation:firefox" "jacoblincool/bahamut-automation:firefox-$version"
 docker tag "jacoblincool/bahamut-automation:chromium" "jacoblincool/bahamut-automation:chromium-$version"
