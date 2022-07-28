22f07039908bc9e18b37f013696ad8c14b3ae9de
JacobLinCool <jacoblincool@gmail.com>
2022-07-29 02:46:03 +0800

    build: force failed docker publish script

--- scripts/docker_dev.sh
+++ scripts/docker_dev.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-docker buildx bake --push --set "*.platform=linux/arm/v7,linux/arm64/v8,linux/amd64" --file docker-compose.dev.yml
+set -e
 commit=$(git rev-parse --short main)
-docker pull jacoblincool/bahamut-automation:dev-pure
+docker buildx bake --push --set "*.platform=linux/arm/v7,linux/arm64/v8,linux/amd64" --file docker-compose.dev.yml
 docker pull jacoblincool/bahamut-automation:dev-firefox
 docker pull jacoblincool/bahamut-automation:dev-chromium
 docker pull jacoblincool/bahamut-automation:dev-webkit
 docker pull jacoblincool/bahamut-automation:dev-all
-docker tag "jacoblincool/bahamut-automation:dev-pure" "jacoblincool/bahamut-automation:pure-$commit"
 docker tag "jacoblincool/bahamut-automation:dev-firefox" "jacoblincool/bahamut-automation:firefox-$commit"
 docker tag "jacoblincool/bahamut-automation:dev-chromium" "jacoblincool/bahamut-automation:chromium-$commit"
 docker tag "jacoblincool/bahamut-automation:dev-webkit" "jacoblincool/bahamut-automation:webkit-$commit"
--- scripts/docker_release.sh
+++ scripts/docker_release.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-docker buildx bake --push --set "*.platform=linux/arm/v7,linux/arm64/v8,linux/amd64"
+set -e
 version=$(git describe --tags --abbrev=0)
-docker pull jacoblincool/bahamut-automation:pure
+docker buildx bake --push --set "*.platform=linux/arm/v7,linux/arm64/v8,linux/amd64"
 docker pull jacoblincool/bahamut-automation:firefox
 docker pull jacoblincool/bahamut-automation:chromium
 docker pull jacoblincool/bahamut-automation:webkit
 docker pull jacoblincool/bahamut-automation:all
-docker tag "jacoblincool/bahamut-automation:pure" "jacoblincool/bahamut-automation:pure-$version"
 docker tag "jacoblincool/bahamut-automation:firefox" "jacoblincool/bahamut-automation:firefox-$version"
 docker tag "jacoblincool/bahamut-automation:chromium" "jacoblincool/bahamut-automation:chromium-$version"
 docker tag "jacoblincool/bahamut-automation:webkit" "jacoblincool/bahamut-automation:webkit-$version"
