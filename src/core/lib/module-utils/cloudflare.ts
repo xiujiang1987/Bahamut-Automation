1fdd18689441dd26aacf299296c826c2a94cdf0a
JacobLinCool <jacoblincool@gmail.com>
2022-08-11 04:23:43 +0800

    fix: wait cloudflare for more time

--- src/core/lib/module-utils/cloudflare.ts
+++ src/core/lib/module-utils/cloudflare.ts
@@ -1,11 +1,9 @@
 import { Page } from "playwright-core";
 import { VERBOSE } from "../constants.js";
 
-export async function wait_for_cloudflare(page: Page) {
-    const spiiner = await page.$("#cf-spinner-please-wait");
-
-    if (spiiner === null) {
-        return;
+export async function wait_for_cloudflare(page: Page, tried = 0) {
+    if (tried > 5) {
+        throw new Error("等待 Cloudflare 失敗");
     }
 
     const blocked = await page.isVisible("#cf-spinner-please-wait");
@@ -18,5 +16,7 @@ export async function wait_for_cloudflare(page: Page) {
         await page.waitForNavigation({ timeout: 10_000 }).catch(() => {
             throw new Error("Cloudflare blocked (auto navigation timeout)");
         });
+
+        await wait_for_cloudflare(page, tried + 1);
     }
 }
